7de69c1716a0c14a864ef2d72bae04858066702d
Younghak Shin <yshin@c2r10s8.42seoul.kr>
2021-06-07 14:53:36 +0900

    test: step1 test

--- cypress/integration/step1_test.ts
+++ cypress/integration/step1_test.ts
@@ -35,12 +35,66 @@ describe("step1 test", () => {
   //   // 추가 검색 및 결과 출력 여부 확인 필요
   // });
 
-  it("'저장 버튼'을 클릭하면, '저장 버튼'이 삭제된다.", () => {
-    cy.get("#search-button").click();
-    cy.get("#search-input").type("축구");
-    cy.get("#search-submit-button").click();
-    cy.get("div.modal article:nth-child(1) button").click();
-    cy.get("div.modal article:nth-child(1)").children('button').should("not.exist");
-  });
+  // it("'저장 버튼'을 클릭하면, '저장 버튼'이 삭제된다.", () => {
+  //   cy.get("#search-button").click();
+  //   cy.get("#search-input").type("축구");
+  //   cy.get("#search-submit-button").click();
+  //   cy.get("div.modal article:nth-child(1) button").click();
+  //   cy.get("div.modal article:nth-child(1)").children('button').should("not.exist");
+  // });
 
+  // it("'저장 버튼'을 클릭하면 해당 영상 id가 웹 스토리지에 저장된다.", () => {
+  //   cy.get("#search-button").click();
+  //   cy.get("#search-input").type("축구");
+  //   cy.get("#search-submit-button").click();
+  //   cy.get("div.modal article:nth-child(1) button").click();
+  //   //const temp = localStorage.getItem("data");
+  //   //expect(temp).to.not.equal('temp');
+  // }); // 수정 필요
+
+  // it("저장된 영상의 최대 갯수는 100개이다.", () => {
+  //   cy.get("#search-button").click();
+  //   cy.get("#search-input").type("축구");
+  //   cy.get("#search-submit-button").click();
+  //   for (let i = 1; i <= 101; i++) {
+  //     if (i % 10 === 0) {
+  //       cy.get("div.modal-inner").scrollTo("bottom");
+  //     }
+  //     cy.get(`div.modal article:nth-child(${i}) button`).click();
+  //   }
+  //   // const numOfSavedItem = localStorage.getItem('savedItemCount');
+  //   // expect(numOfSavedItem).to.equal(100);
+  // }); // 수정 및 확인 필요
+
+  // it("검색을 진행했을 때, 검색어가 최근 검색어에 포함된다.", () => {
+  //   cy.get("#search-button").click();
+  //   cy.get("#search-input").type("축구");
+  //   cy.get("#search-submit-button").click();
+  //   cy.get("div.modal section:nth-child(4) a:nth-child(2)").should("have.text", "축구");
+  // });
+
+  // it("검색 모달이 다시 로딩된 경우, 최근에 검색한 결과를 보여준다.", () => {
+  //   cy.get("#search-button").click();
+  //   cy.get("#search-input").type("축구");
+  //   cy.get("#search-submit-button").click();
+  //   cy.get("path.close-x").click();
+  //   cy.get("#search-button").click();
+  //   cy.get("#search-input").should("have.value", "축구");
+  // });
+
+  // it("검색을 4번 진행했을 때, 가장 최근의 3개 검색어를 보여준다.", ()=>{
+  //   cy.get("#search-button").click();
+  //   cy.get("#search-input").type("축구");
+  //   cy.get("#search-submit-button").click();
+  //   cy.get("#search-input").type("야구");
+  //   cy.get("#search-submit-button").click();
+  //   cy.get("#search-input").type("농구");
+  //   cy.get("#search-submit-button").click();
+  //   cy.get("#search-input").type("배구");
+  //   cy.get("#search-submit-button").click();
+  // cy.get("div.modal section:nth-child(4)").children().should("have.length", 4);
+  //   cy.get("div.modal section:nth-child(4) a:nth-child(2)").should("have.text", "배구");
+  //   cy.get("div.modal section:nth-child(4) a:nth-child(3)").should("have.text", "농구");
+  //   cy.get("div.modal section:nth-child(4) a:nth-child(4)").should("have.text", "야구");
+  // });
 });
