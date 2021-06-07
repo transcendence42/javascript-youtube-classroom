196db6a8c088a185c2f3b7e7dafcd87e7c55af91
Yeji Choi <yechoi@student.42seoul.kr>
2021-06-07 13:31:22 +0900

    test: describe, it 작성

--- src/js/index.ts
+++ src/js/index.ts
@@ -16,5 +16,6 @@ $searchButton?.addEventListener('click', onModalShow);
 $modalClose?.addEventListener('click', onModalClose);
 
 fetch(
-  `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${ENV.API_KEY}&part=snippet,contentDetails,statistics,status`,
-).then((res) => console.log(res));
+  `https://www.googleapis.com/youtube/v3/search?key=${ENV.API_KEY}&part=snippet&q=bts&maxResults=10&type=video&videoEmbeddable=true`,
+).then((resp)=> resp.json())
+.then((items) =>console.log(items))
