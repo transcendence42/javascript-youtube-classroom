d4d4a680cb6a8ad675450865c70b58574e002bab
jwon42 <jwon@student.42seoul.kr>
2021-06-08 16:40:43 +0900

    feat: add recent keyword

--- src/ts/view/modalPage/modalRecentKeywordSection.ts
+++ src/ts/view/modalPage/modalRecentKeywordSection.ts
@@ -1,8 +1,5 @@
 export const modalRecentKeywordSection = (): string => {
-  return `<section class="mt-2">
+  return `<section class="mt-2" id="recent-keyword">
   <span class="text-gray-700">최근 검색어: </span>
-  <a class="chip">${'메이커준'}</a>
-  <a class="chip">${'우아한테크코스'}</a>
-  <a class="chip">${'우아한형제들'}</a>
 </section>`;
 };
