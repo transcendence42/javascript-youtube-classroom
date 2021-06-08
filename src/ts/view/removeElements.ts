4dbbbc43ae4b42692d74fcda809a6ffcd3170213
jwon42 <jwon@student.42seoul.kr>
2021-06-08 18:06:15 +0900

    feat: add not found result

---
+++ src/ts/view/removeElements.ts
@@ -0,0 +1,23 @@
+import { $, $$ } from '../util.js';
+
+export const removeModalArticles = () => {
+  $("div.modal-inner section.video-wrapper")!.innerHTML = "";
+}
+
+export const removeNotFoundImage = () => {
+  $('div.modal section img')?.remove();
+}
+
+export const removeDuplicateRecentKeyword = (searchValue: string) => {
+  ($$(".chip") as NodeListOf<HTMLAnchorElement>).forEach((elem)=>{
+    if (elem.innerText === searchValue) {
+      elem.remove();
+    }
+  });
+}
+
+export const removeOldSearchKeyword = () => {
+  if (($$('.chip') as NodeList).length === 4) {
+    $('a.chip:last-child')?.remove();
+  }
+}
--- src/ts/view/renderElements.ts
+++ src/ts/view/renderElements.ts
@@ -2,8 +2,10 @@ import { $, $$ } from '../util.js';
 // import {titleSection, articleSection, article, modal} from './mainPage/index.js';
 import { modalArticle } from './modalPage/index.js';
 import { IVideoInfo } from '../model/IVideoInfo.js';
+import { removeModalArticles, removeNotFoundImage } from './removeElements.js';
 
 export const renderSearchedArticle = (videoInfo: IVideoInfo) => {
+  removeNotFoundImage();
   const $articleSection: HTMLElement = $("div.modal-inner section.video-wrapper") as HTMLElement;
   $articleSection.insertAdjacentHTML("beforeend", modalArticle(videoInfo));
 }
@@ -11,4 +13,9 @@ export const renderSearchedArticle = (videoInfo: IVideoInfo) => {
 export const renderRecentKeyword = (searchValue: string) => {
   const $recentKeywordSpan: HTMLSpanElement = $("#recent-keyword > span") as HTMLSpanElement;
   $recentKeywordSpan.insertAdjacentHTML("afterend", `<a class="chip">${searchValue}</a>`);
+}
+
+export const renderNotFoundImage = () => {
+  $("div.modal-inner section.video-wrapper")?.insertAdjacentHTML("beforebegin", "<img src='./src/images/status/not_found.png' />");
+  removeModalArticles();
 }
