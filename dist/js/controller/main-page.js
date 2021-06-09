154b67c15052ee16356ddf2bbd1001d84dddbfe8
Yeji Choi <yechoi@student.42seoul.kr>
2021-06-09 15:15:36 +0900

    feat: 볼 영상 버튼 누를 시 다른 페이지 삭제 및 볼 영상 렌더링

--- dist/js/controller/main-page.js
+++ dist/js/controller/main-page.js
@@ -1,6 +1,11 @@
 import { $ } from '../@shared/utils/utils.js';
 import { clickCheckButtons } from './index.js';
+import { renderMainPage } from '../view/main-page.js';
+const clickMainPageButton = () => {
+    renderMainPage();
+};
 export const mainPageController = () => {
-    var _a;
+    var _a, _b;
     (_a = $('#main-videos')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', clickCheckButtons);
+    (_b = $('#main-page-button')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', clickMainPageButton);
 };
--- src/js/controller/main-page.ts
+++ src/js/controller/main-page.ts
@@ -1,6 +1,12 @@
 import { $ } from '../@shared/utils/utils.js';
 import { clickCheckButtons } from './index.js';
+import { renderMainPage } from '../view/main-page.js';
+
+const clickMainPageButton = () => {
+  renderMainPage();
+};
 
 export const mainPageController = () => {
   $('#main-videos')?.addEventListener('click', clickCheckButtons);
+  $('#main-page-button')?.addEventListener('click', clickMainPageButton);
 };
