3fc40c7751fe84d5253ce118c581450312ac2fcd
Yeji Choi <yechoi@student.42seoul.kr>
2021-06-14 13:22:19 +0900

    feat: 반응형 css

--- src/js/@shared/constants/env.ts
+++ src/js/@shared/constants/env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
-  API_KEY: 'AIzaSyBOh_tyun6L8f4ApgQc0JfdbeXq-k2BCHM',
-  API_KEY_2: 'AIzaSyBs8RgFyiz1mghVCWvaAB0TboXHwS73-50',
+  API_KEY: 'AIzaSyBs8RgFyiz1mghVCWvaAB0TboXHwS73-50',
+  API_KEY_2: 'AIzaSyBOh_tyun6L8f4ApgQc0JfdbeXq-k2BCHM',
   YOUTUBE_SEARCH_URL: `https://www.googleapis.com/youtube/v3/search?`,
   YOUTUBE_WATCH_URL: `https://www.youtube.com/embed/`,
   YOUTUBE_CHANNEL_URL: `https://www.youtube.com/channel/`,
